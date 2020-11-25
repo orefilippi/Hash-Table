7bc0924281404e7ec7b796feadf22b3f850aea09
orestis <orestisfilippi@gmail.com>
2020-11-25 12:05:29 +0000

    adding data

---
+++ hash_table.asm
@@ -0,0 +1,15 @@
+    .data
+
+menu: .asciiz "\n Menu\n1. Insert Key\n2. Find Key\n3. Display Hash Table\n4. Exit"
+choise: .asciiz "\nChoise? "
+readKey: .asciiz "\nGive new key (greater than zero): "
+wrongKey: .asciiz "\nkey must be greater than zero"
+readKeyFor: .asciiz "\nGive key to search for: "
+notFindKey: .asciiz "\nKey not in hash table"
+keyValue: .asciiz "Key value = "
+tablePos: .asciiz "Table position = \n"
+alreadyKey: .asciiz "\nKey is already in hash table."
+fullTable: .asciiz "\nhash table is full"
+posKey: .asciiz "\npos key\n"
+quotes: .asciiz " "
+newLine: .asciiz "\n"
